0512cb4c7d4d8b3aa7519052613d25ba3e9b225f
mraamr <mraamr899@gmail.com>
2023-09-18 14:29:05 +0500

    hello world

---
+++ dars-9 (array)-2/script.js
@@ -0,0 +1,114 @@
+// join()
+// concat()
+// unshift()
+// toString()
+// pop()
+// shift()
+
+// -------------------------------Array Methods (2)---------------------------------
+
+
+//1)---------------------------------splice-----------------------------------------
+
+//              0        1        2         3       4
+// let array = ['apple', 'mango', 'banana', 'lemon', 'kiwi'];
+
+// array.splice(1, 3);
+
+// console.log(array); // apple kiwi
+
+
+
+// -----------------------------------slice--------------------------------------------
+
+
+// let array = ['apple', 'mango', 'banana', 'lemon', 'kiwi'];
+
+// array.splice(1, 3); // 'mango', 'banana', 'lemon'
+// array.slice(1, 2); // 'mango'
+// console.log(array.slice(1, 1));
+
+
+
+
+// ------------------------------------IndexOf------------------------------
+
+// let arr = ['apple', 'mango', 'banana', 'lemon', 'kiwi'];
+
+// console.log(arr.indexOf('mango')); // 1
+
+
+
+// let array1 = ['apple', 'mango', 'banana', 'lemon', 'kiwi'];
+// let array2 = ['damas', 'tiko', 'matiz', 'spark', 'nexia'];
+// let array3 = ['eshmat', 'toshmat', 'ketmon', 'tesha', 'qilich'];
+
+// let newArr = array1.concat(array2, array3)
+
+// newArr.splice(newArr.indexOf('spark'), 1)
+
+// console.log(newArr);
+
+
+
+
+// ------------------------------------LastIndexOf------------------------------
+
+// let arr = ['apple', 'mango', 'banana', 'lemon', 'kiwi','mango'];
+
+// console.log(arr.lastIndexOf('mango')); // 5
+
+
+
+
+
+// -----------------------------includes---------------------------
+
+// let fruits = ['apple', 'oranges', 'banana', 'kiwi', 'melon'];
+
+// console.log(fruits.includes('spark')); // false
+
+
+// Array.isArray()
+
+
+// let numArr = [40, 20, 54, 89, 64, 100, 2, 5];
+
+// 64 => 98, 99;
+// [20, 2];
+
+
+
+// ----------------------------------split--------------------------
+
+
+// let arr = ['apple', 'mango', 'banana', 'lemon', 'kiwi'];
+
+// let b = arr.join('-')
+
+// console.log(b);
+
+// let c = b.split('-')
+
+// console.log(c);
+
+// let str = '4*54*90*12*19*43';
+
+// let matn = 'abcde' // 'a','b','c','d','e'
+
+
+
+
+// ----------------------------------reverse--------------------------
+
+
+// let arr = ["apple", "mango", "banana", '1', '2', '4'];
+
+// arr.reverse()
+
+// console.log(arr.indexOf('apple')); //5
+
+
+// ----------------------------advanced array methods-------------------------
+
+
