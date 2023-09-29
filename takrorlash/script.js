e0b7ba50d0faa9065edae7fc95a8c8244bacfee3
mraamr <114140248+Mrshokhrukh@users.noreply.github.com>
2023-09-29 16:52:37 +0500

    фввув

---
+++ takrorlash/script.js
@@ -0,0 +1,68 @@
+// let son = prompt('Son kiriting')
+// whileF (son > 0) {
+//     son = prompt('Son kiriting')
+// }
+
+// --------------------------
+
+// let salom = []
+// let a = prompt('son kiriting');
+// while (a !== '0') {
+//     salom.push(a);
+//     a = prompt('son kiriting');
+// }
+// let result = 0;
+// if (a == 0) {
+//     for (let i = 0; i < salom.length; i++) {
+//         result += +salom[i];
+//     }
+// }
+
+
+// do{
+//     a=prompt('harf kirit ')
+// }while(a!=='x')
+
+
+// let son = prompt('son kiriting: ');
+
+// for (let i = 1; i <= son; i++) {
+//     console.log(i);
+// }
+
+
+// let son = prompt('son kiriting')
+// if (son > 100) {
+//     for (let i = 100; i <= son; i++) {
+//         console.log(i);
+//     }
+
+// } else {
+//     for (let i = 100; i >= son; i--) {
+//         console.log(i);
+//     }
+// }
+
+
+
+// let soatlar = []
+// let ishlanganSoat = parseInt(prompt('soatni kirinting'))
+// let count = 0;
+// let umumiyOylikSummasi = 0;
+
+// while (count < 10) {
+//     count++;
+//     soatlar.push(ishlanganSoat);
+//     ishlanganSoat = parseInt(prompt(`${count}-inchi ishchi uchun soat kiriting`));
+
+// }
+// let maosh = null;
+// let sum = 0;
+
+// if (count == 10) {
+//     for (let i = 0; i < soatlar.length; i++) {
+//         console.log(`${i + 1}-ishchi oylik maoshi: ${maosh = soatlar[i] * 50}`);;
+//         sum += maosh;
+//     }
+//     console.log('umumiy summa ', sum);
+// }
