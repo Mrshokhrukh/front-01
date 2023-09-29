46307c63b444f5349a222f66b34eec924b418dcf
mraamr <114140248+Mrshokhrukh@users.noreply.github.com>
2023-09-29 14:53:25 +0500

    tic tac toe

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,9 +1,12 @@
 let Elements = document.querySelectorAll('[data_item]')
-
+let turn = document.querySelector('.turn')
 let X_TURN = 'X';
 let CIRCLE_TURN = 'O';
+let currentTurn;
+let winningTextClass = document.querySelector('.winning_text');
+let winningText = document.querySelector('.winning_text h2');
+let playerTurn = X_TURN;
 
-let playerTurn;
 
 let winningCominations = [
     [0, 1, 2],
@@ -22,20 +25,47 @@ startGame();
 
 function startGame() {
     Elements.forEach(v => {
+        winningTextClass.classList.remove('show');
+        v.classList.remove(X_TURN)
+        v.classList.remove(CIRCLE_TURN)
+        v.innerHTML = ''
         v.addEventListener('click', handleClick, { once: true });
     })
+
 }
 
+
 function handleClick(e) {
+    turn.innerHTML = `${playerTurn} turn`
     playerTurn = playerTurn === X_TURN ? CIRCLE_TURN : X_TURN;
     let clickedBox = e.target;
     selected(clickedBox, playerTurn);
+    currentTurn = currentTurn == X_TURN ? CIRCLE_TURN : X_TURN
+
 
     if (isWon()) {
-        console.log('asd,,,');
+        gameOver(currentTurn)
+    } else {
+        if (isDraw()) {
+            turn.innerHTML = `draw`
+            winningText.innerHTML = `draw`
+            winningTextClass.classList.add('show');
+        }
     }
+}
 
 
+function isDraw() {
+    return winningCominations.every((v, i) => {
+        return Elements[i].classList.contains(X_TURN) || Elements[i].classList.contains(CIRCLE_TURN)
+    })
+}
+
+function gameOver(won) {
+    if (won) {
+        winningText.innerHTML = `${currentTurn} is won`
+        winningTextClass.classList.add('show');
+    }
 }
 
 function selected(box, playerTurn) {
