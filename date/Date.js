56b29c0df498e0ca077df9f7c433976c0b7eff34
mraamr <114140248+Mrshokhrukh@users.noreply.github.com>
2023-12-20 14:35:52 +0500

    local storage

--- date/Date.js
+++ date/Date.js
@@ -2,6 +2,7 @@ let text = document.querySelector('h2');
 let input = document.getElementById('date');
 
 let weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
 // year: ${date.getFullYear()} <br>
 // month: ${date.getMonth() + 1} <br>
 // day: ${date.getDate()} <br>
@@ -23,3 +24,31 @@ function showDate() {
     text.innerHTML = '15:28:34'
 };
 
+
+// let body = document.body;
+// let interval = null;
+
+// let intFunc = (para) => {
+//     if (para == 'cont') {
+//         interval = setInterval(() => {
+//             startChanging();
+//         }, 1000);
+//     }
+//     if (para == 'stop') {
+//         console.log('stopped');
+//         clearInterval(interval);
+//     }
+// }
+// function start() {
+//     intFunc('cont');
+// }
+// function stop() {
+//     intFunc('stop')
+// }
+
+// function startChanging() {
+//     let red = Math.floor(Math.random() * 255);
+//     let green = Math.floor(Math.random() * 255);
+//     let blue = Math.floor(Math.random() * 255);
+//     body.style.backgroundColor = `rgb(${red},${green},${blue})`
+// }
---
+++ webApi'sStorages/script.js
@@ -0,0 +1,39 @@
+// let img = document.querySelector('img');
+// let src = 'https://c4.wallpaperflare.com/wallpaper/149/17/318/ice-4k-hd-quality-desktop-wallpaper-preview.jpg'
+// imgFunc();
+// function imgFunc() {
+//     let storage = JSON.parse(sessionStorage.getItem('src'));
+//     img.src = (storage ? storage : 'https://fomantic-ui.com/images/wireframe/image.png');
+// }
+// function addSrc() {
+//     sessionStorage.setItem('src', JSON.stringify(src));
+//     imgFunc();
+// }
+// function deleteSrc() {
+//     sessionStorage.removeItem('src');
+//     imgFunc()
+// }
+
+
+
+
+// localStorages & sessionStorages
+
+
+// setItem
+
+// let arr = ['matiz', 'spark']
+
+// localStorage.setItem('ism', JSON.stringify(arr))
+// localStorage.setItem('name', 'eshmat')
+// localStorage.setItem('name1', 'toshmat')
+// // getItem
+
+// let res = JSON.parse(localStorage.getItem('ism'));
+
+
+// // removeItem
+
+
+// // clear
+// localStorage.clear()
